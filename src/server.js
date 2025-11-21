2330315fbc313a5db2f8c4763e387074f85f94de
Svarium <you@example.com>
2025-11-20 21:33:59 -0300

    add productRoutes

--- src/server.js
+++ src/server.js
@@ -1,9 +1,20 @@
 const express = require("express");
 require("dotenv").config();
-
+const morgan = require("morgan");
+//IMPORTAR LAS RUTAS
+const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 
+//MIDDLEWARES
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true})); //PARA QUE EXPRESS PUEDA LEER LOS DATOS DE FORMULARIOS
+
+//ROUTES
+app.use("/api/v1/products", productRoutes);
+
+//PUERTO
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`)
